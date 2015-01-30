284f0bd924caa8779f255f786b70ca24411fd710
Kat <kmmpatke@gmail.com>
2015-01-30 14:17:42 -0800

    split up dataset layers, set weights for diff layers, heat map works (except bars)

--- app/js/controllers/home.js
+++ app/js/controllers/home.js
@@ -13,17 +13,60 @@ homeControllerModule.controller('homeController', ['$scope', '$http',
 
     $http.get('http://localhost:3000/').success(function(data) {
       $scope.noiseArray = data;
-      var heatmapData = [];
-      for(var i = 0; i < $scope.noiseArray.length; i += 1) {
+      var allLayers = {
+        fireStations: [],
+        colleges: [],
+        schools: [],
+        trolley: [],
+        buses: [],
+        hospitals: [],
+        bars: [],
+        policeStations: [],
+        transitCenters: [],
+        dumps: []
+      };
+
+      for(var i = 0; i < $scope.noiseArray.length; i++) {
         var location = $scope.noiseArray[i];
         var latLon = new google.maps.LatLng(location.lat, location.lon);
-        heatmapData.push(latLon);
+        if (location.noise_type === "Transit Center"){
+          allLayers.transitCenters.push({location: latLon, weight: 11});
+          }
+        else if (location.noise_type === "Dump") {
+          allLayers.dumps.push({location: latLon, weight: 10});
+        }
+        else if (location.noise_type === "Fire Station"){
+          allLayers.fireStations.push({location: latLon, weight: 14});
+        }
+        else if (location.noise_type === "College"){
+          allLayers.colleges.push({location: latLon, weight: 11});
+        }
+        else if (location.noise_type === "School"){
+          allLayers.schools.push({location: latLon, weight: 9});
+        }
+        else if (location.noise_type === "Trolley"){
+          allLayers.trolley.push({location: latLon, weight: 8});
+        }
+        else if (location.noise_type === "Bus"){
+          allLayers.buses.push({location: latLon, weight: 10});
+        }
+        else if (location.noise_type === "Police Station"){
+          allLayers.policeStations.push({location: latLon, weight: 14});
+        }
+        else if (location.noise_type === "Hospital"){
+          allLayers.hospitals.push({location: latLon, weight: 14});
+        }
+        else if (location.noise_type === "Bars"){
+          allLayers.bars.push({location: latLon, weight: 10});
+        }
       }
 
-      var heatmap = new google.maps.visualization.HeatmapLayer({
-        data: heatmapData
-      });
-      heatmap.setMap($scope.map);
+      for (var heatmapData in allLayers){
+        var heatmap = new google.maps.visualization.HeatmapLayer({
+          data: heatmapData
+        });
+        heatmap.setMap($scope.map);
+      }
     });
 
 
