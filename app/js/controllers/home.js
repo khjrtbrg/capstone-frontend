572c373f8cf73244da10e63f4bf0cf355171777f
Kristina Hjertberg <hjert.k@gmail.com>
2015-01-29 22:12:56 -0800

    initial successful tests, http request for noiseArray

--- app/js/controllers/home.js
+++ app/js/controllers/home.js
@@ -4,4 +4,8 @@ homeControllerModule.controller('homeController', ['$scope', '$http',
   function($scope, $http) {
     $scope.hello = 'Noise!!';
 
+    $http.get('http://localhost:3000/').success(function(data) {
+      $scope.noiseArray = data;
+    });
+
   }]);
--- test/unit/homeSpec.js
+++ test/unit/homeSpec.js
@@ -1,14 +1,25 @@
 'use strict';
 
-/* jasmine specs for controllers go here */
 describe('Home Controllers', function() {
 
   describe('homeController', function(){
 
     var scope, ctrl, $httpBackend;
+    var noise_array = [{
+          noise_type: 'Traffic',
+          lat: 44,
+          lon: 122,
+          decibel: 50,
+          reach: 100,
+          seasonal: false
+        }];
 
     beforeEach(module('noiseScoreApp'));
-    beforeEach(inject(function($rootScope, $controller) {
+    beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
+      $httpBackend = _$httpBackend_;
+      $httpBackend.expectGET('http://localhost:3000/').
+        respond(noise_array);
+
       scope = $rootScope.$new();
       ctrl = $controller('homeController', {$scope: scope});
     }));
@@ -17,6 +28,11 @@ describe('Home Controllers', function() {
     it('should assign $scope.hello', function() {
       expect(scope.hello).toEqual('Noise!!');
     });
+
+    it('should assign $scope.noiseArray', function() {
+      $httpBackend.flush();
+      expect(scope.noiseArray).toEqual(noise_array);
+    })
 
   });
 });
