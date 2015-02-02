01d9594c2c90f4e55458969018c236b66404b609
Kristina Hjertberg <hjert.k@gmail.com>
2015-02-01 19:29:19 -0800

    moving layer setup into service

--- app/js/controllers/home.js
+++ app/js/controllers/home.js
@@ -1,83 +1,42 @@
 var homeControllerModule = angular.module('homeControllerModule', []);
 
-homeControllerModule.controller('homeController', ['$scope', '$http', 'sortLayerArrays',
-  function($scope, $http, sortLayerArrays) {
+homeControllerModule.controller('homeController', ['$scope', '$http', 'layerService', 'locationService',
+  function($scope, $http, layerService, locationService) {
 
     var mapOptions = {
       center: { lat: 47.6, lng: -122.3},
       zoom: 13
     };
 
-    var marker;
-    var markers = [];
-
     // Create & Add Map
     $scope.map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
     // Fetch Noises From API and Process Into Layers
     $http.get('http://localhost:3000/').success(function(data) {
-      var allLayers = {
-        fireStations: [],
-        colleges: [],
-        schools: [],
-        hospitals: [],
-        bars: [],
-        policeStations: [],
-        transit: [],
-        dumps: []
-      };
 
-      // Sort Data into allLayers
-      sortLayerArrays.go(data, allLayers);
-      
-      // Create Heatmap Layer Function
-      var createLayer = function(heatmapName) {
-        $scope[heatmapName] = new google.maps.visualization.HeatmapLayer({
-          data: allLayers[heatmapName]
-        });
-        
-        $scope[heatmapName].setMap($scope.map);
+      // Sort Data into Layer Arrays
+      var layers = layerService.setupLayers(data);
+
+      // Create Heatmap Layers from Layer Arrays
+      for (var heatmapData in layers){
+        layerService.createLayer($scope, heatmapData, layers);
       }
 
       // Toggle Layer Function
       $scope.toggleLayer = function(layerName) {
         layerName.setMap(layerName.getMap() ? null : $scope.map);
       }
-
-      // Create Heatmap Layers from allLayers
-      for (var heatmapData in allLayers){
-        createLayer(heatmapData);
-      }
     });
 
-    var clearMarkers = function() {
-      for (var i = 0; i < markers.length; i++) {
-        markers[i].setMap(null);
-      }
-      markers = [];
-    };
-
-    var createMarker = function(coordinates) {
-      marker = new google.maps.Marker({
-        position: coordinates,
-        map: $scope.map
-      });
-
-      markers.push(marker);
-    }
-
-    var addMarkerToMap = function() {
-      $scope.map.setZoom(15);
-      $scope.map.setCenter(marker.getPosition());
-    };
+    // Zoom Map to Searched Location
+    var markers = [];
 
     $scope.findLocation = function() {
       var url = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + $scope.locationSearch + ',+Seattle,+WA&key=APIKEYHERE';
       
       $http.get(url).success(function(data) {
-        clearMarkers();
-        createMarker(data.results[0].geometry.location);
-        addMarkerToMap();
+        var coordinates = data.results[0].geometry.location;
+        locationService.newMarker(coordinates, $scope, markers);
       });
     }
   }]);
--- app/js/services.js
+++ app/js/services.js
@@ -1,37 +1,80 @@
 var servicesModule = angular.module('servicesModule', []);
 
-servicesModule.factory('sortLayerArrays', function() {
-  return { 
-    go: function(noiseArray, allLayers) {
-      for (var i = 0; i < noiseArray.length; i++) {
-        var location = noiseArray[i];
+servicesModule.factory('layerService', function() {
+  return {
+    setupLayers: function(apiResponse) {
+      var layers = {
+        fireStations: [],
+        colleges: [],
+        schools: [],
+        hospitals: [],
+        bars: [],
+        policeStations: [],
+        transit: [],
+        dumps: []
+      };
+
+      for (var i = 0; i < apiResponse.length; i++) {
+        var location = apiResponse[i];
         var type = location.noise_type;
         var latLon = new google.maps.LatLng(location.lat, location.lon);
         if (type === "Transit Center" || type === "Bus Stop" || type === "Trolley"){
-          allLayers.transit.push({location: latLon, weight: 11});
+          layers.transit.push({location: latLon, weight: 11});
         }
         else if (type === "Dump") {
-          allLayers.dumps.push({location: latLon, weight: 10});
+          layers.dumps.push({location: latLon, weight: 10});
         }
         else if (type === "Fire Station") {
-          allLayers.fireStations.push({location: latLon, weight: 14});
+          layers.fireStations.push({location: latLon, weight: 14});
         }
         else if (type === "College") {
-          allLayers.colleges.push({location: latLon, weight: 11});
+          layers.colleges.push({location: latLon, weight: 11});
         }
         else if (type === "School") {
-          allLayers.schools.push({location: latLon, weight: 9});
+          layers.schools.push({location: latLon, weight: 9});
         }
         else if (type === "Police Station") {
-          allLayers.policeStations.push({location: latLon, weight: 14});
+          layers.policeStations.push({location: latLon, weight: 14});
         }
         else if (type === "Hospital") {
-          allLayers.hospitals.push({location: latLon, weight: 14});
+          layers.hospitals.push({location: latLon, weight: 14});
         }
         else if (type === "Bars") {
-          allLayers.bars.push({location: latLon, weight: 10});
+          layers.bars.push({location: latLon, weight: 10});
         }
       }
+
+      return layers;
+    },
+    createLayer: function(scope, heatmapName, allLayers) {
+      scope[heatmapName] = new google.maps.visualization.HeatmapLayer({
+        data: allLayers[heatmapName]
+      });
+      
+      scope[heatmapName].setMap(scope.map);
+    }
+  }
+});
+
+servicesModule.factory('locationService', function() {
+  return {
+    newMarker: function(coordinates, scope, markers) {
+      // clear any current markers
+      for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(null);
+      }
+      markers = [];
+
+      // create new marker
+      marker = new google.maps.Marker({
+        position: coordinates,
+        map: scope.map
+      });
+      markers.push(marker);
+
+      // zoom to new marker
+      scope.map.setZoom(15);
+      scope.map.setCenter(marker.getPosition());
     }
   }
 });
