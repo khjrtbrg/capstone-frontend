486920c24ff337d6adb6e9ffb62a3a45ee50a573
Kat <kmmpatke@gmail.com>
2015-02-03 23:03:55 -0800

    radius adjustment according to zoom is...kind of working. Ish.

--- app/js/services.js
+++ app/js/services.js
@@ -4,17 +4,18 @@ servicesModule.factory('layerService', function() {
   return {
     setupLayers: function(apiResponse) {
       var layers = {
-        fireStations: {radius: 15, items: []},
-        colleges: {radius: 16, items: []},
-        schools: {radius: 10, items: []},
-        hospitals: {radius: 25, items: []},
-        bars: {radius: 10, items: []},
-        policeStations: {radius: 15, items: []},
-        transit: {radius: 10, items: []},
-        dumps: {radius: 15, items: []},
-        construction: {radius: 30, items: []},
-        demolition: {radius: 30, items: []}
+        // fireStations: {radius: 15750, items: []},
+        // colleges: {radius: 1000, items: []},
+        // schools: {radius: 1000, items: []},
+        // hospitals: {radius: 15750, items: []},
+        // bars: {radius: 1000, items: []},
+        // policeStations: {radius: 15750, items: []},
+        transit: {radius: 10000, items: []},
+        // dumps: {radius: 1500, items: []},
+        // construction: {radius: 9054, items: []},
+        // demolition: {radius: 15750, items: []}
       };
+
 
       // Add API Response Data to Layers
       for (var i = 0; i < apiResponse.length; i++) {
@@ -24,42 +25,44 @@ servicesModule.factory('layerService', function() {
         if (type === "Transit Center" || type === "Bus Stop" || type === "Trolley"){
           layers.transit.items.push({location: latLon, weight: 11});
         }
-        else if (type === "Dump") {
-          layers.dumps.items.push({location: latLon, weight: 10});
-        }
-        else if (type === "Fire Station") {
-          layers.fireStations.items.push({location: latLon, weight: 14});
-        }
-        else if (type === "College") {
-          layers.colleges.items.push({location: latLon, weight: 11});
-        }
-        else if (type === "School") {
-          layers.schools.items.push({location: latLon, weight: 9});
-        }
-        else if (type === "Police Station") {
-          layers.policeStations.items.push({location: latLon, weight: 14});
-        }
-        else if (type === "Hospital") {
-          layers.hospitals.items.push({location: latLon, weight: 14});
-        }
-        else if (type === "Bar") {
-          layers.bars.items.push({location: latLon, weight: 10});
-        }
-        else if (type === "Construction") {
-          layers.construction.items.push({location: latLon, weight: 16});
-        }
-        else if (type === "Demolition") {
-          layers.demolition.items.push({location: latLon, weight: 16});
-        }
+        // else if (type === "Dump") {
+        //   layers.dumps.items.push({location: latLon, weight: 10});
+        // }
+        // else if (type === "Fire Station") {
+        //   layers.fireStations.items.push({location: latLon, weight: 14});
+        // }
+        // else if (type === "College") {
+        //   layers.colleges.items.push({location: latLon, weight: 11});
+        // }
+        // else if (type === "School") {
+        //   layers.schools.items.push({location: latLon, weight: 9});
+        // }
+        // else if (type === "Police Station") {
+        //   layers.policeStations.items.push({location: latLon, weight: 14});
+        // }
+        // else if (type === "Hospital") {
+        //   layers.hospitals.items.push({location: latLon, weight: 14});
+        // }
+        // else if (type === "Bar") {
+        //   layers.bars.items.push({location: latLon, weight: 10});
+        // }
+        // else if (type === "Construction") {
+        //   layers.construction.items.push({location: latLon, weight: 16});
+        // }
+        // else if (type === "Demolition") {
+        //   layers.demolition.items.push({location: latLon, weight: 16});
+        // }
       }
 
       return layers;
     },
-    createLayer: function(scope, layer, layers) {
+
+    createLayer: function(scope, layer, layers, findRadius) {
+
       // Make Layer & Add to Scope
       scope[layer] = new google.maps.visualization.HeatmapLayer({
         data: layers[layer].items,
-        radius: layers[layer].radius
+        radius: findRadius(layers[layer].radius)       
       });
 
       // Bars Won't Show Up Without maxIntensity
