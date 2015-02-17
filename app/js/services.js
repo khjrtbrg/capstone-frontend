c534a5e3a942cac4e8bf4819f673162e85197b90
Kristina Hjertberg <hjert.k@gmail.com>
2015-02-16 22:54:39 -0800

    adding note to self on line 233 of services.js about radius/reach issue

--- app/js/services.js
+++ app/js/services.js
@@ -183,14 +183,9 @@ servicesModule.factory('newLayerService', function() {
       // Remove Freeways from Data
       var d3Points = [];
       for (var i = 0; i < data.length; i++) {
-        // for testing...
-        if (data[i].noise_type == 'fireStation') {
+        if (data[i].noise_type != 'freeway') {
           d3Points.push(data[i]);
-        }
-
-        // if (data[i].noise_type != 'freeway') {
-        //   d3Points.push(data[i]);
-        // };
+        };
       }
 
       // Add the container when the overlay is added to the map.
@@ -235,6 +230,10 @@ servicesModule.factory('newLayerService', function() {
           function findRadius(d) {
             var reach = d.value.reach;
             return reach / 400;
+            // HERE'S THE PROBLEM! Sirens look great at this reach, everything else is TINY
+            // If we do reach / 50ish, buses look tiny still and sirens overwhelm the city
+            // Buses look best at reach / 25ish, for reference!
+            // Adjust reaches in API?
           }
 
         };
